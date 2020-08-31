231d8ae272a69017eacfffe2412596c4313d3fb4
Ubuntu <ubuntu@ip-172-31-20-36.us-east-2.compute.internal>
2020-08-31 03:41:08 +0000

    Users モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-  get 'users/index'
+  get :signup,       to: 'users#new'
+  resources :users
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
