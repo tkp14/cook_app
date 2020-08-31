602cf42e75a408eda23c9bca753d68925e515cc2
Ubuntu <ubuntu@ip-172-31-20-36.us-east-2.compute.internal>
2020-08-31 16:33:03 +0000

    Finish ユーザー登録処理、プロフィールページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,26 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params:[:id])
   end
 
   def index
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "クック-ウィズへようこそ！"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
   end
 end
