5b66383c4b42eefe3c2898e72ab5ef081474dfd4
Ubuntu <ubuntu@ip-172-31-20-36.us-east-2.compute.internal>
2020-09-01 14:52:17 +0000

    基本的なログイン/ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
-  end
-  
+  def new; end
+
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが誤っています'
+      render 'new'
+    end
   end
-  
+
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
