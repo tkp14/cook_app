6a3e346900241103f0e8720773982622e1cfc319
Ubuntu <ubuntu@ip-172-31-20-36.us-east-2.compute.internal>
2020-09-01 06:38:27 +0000

    Sessionsコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
