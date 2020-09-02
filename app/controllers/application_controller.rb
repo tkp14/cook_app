9cc3b3ce9719db28930460995ec3ecdb6be2545f
Ubuntu <ubuntu@ip-172-31-20-36.us-east-2.compute.internal>
2020-09-02 09:05:03 +0000

    適切なユーザーに適切なアクションを行わせるための実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  private
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
@@ -43,6 +41,7 @@ RSpec.describe 'Users', type: :system do
   
   describe "プロフィール編集ページ" do
     before do
+      login_for_system(user)
       visit user_path(user)
       click_link "プロフィール編集"
     end
@@ -80,6 +79,7 @@ RSpec.describe 'Users', type: :system do
   describe 'プロフィールページ' do
     context 'ページレイアウト' do
       before do
+        login_for_system(user)
         visit user_path(user)
       end
 
