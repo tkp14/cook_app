31c9de5d2121fce5cbe39dbe03ecce8125d0f13b
Ubuntu <ubuntu@ip-172-31-18-217.us-east-2.compute.internal>
2020-09-09 01:25:14 +0000

    Finish 料理モデルの作成

--- spec/factories/dishes.rb
+++ spec/factories/dishes.rb
@@ -1,13 +1,25 @@
 FactoryBot.define do
   factory :dish do
-    name "MyString"
-    description "MyText"
-    portion 1.5
-    tips "MyText"
-    reference "MyText"
-    required_time 1
-    popularity 1
-    cook_memo "MyText"
-    user nil
+    name { Faker::Food.dish }
+    description { "冬に食べたくなる、身体が温まる料理です" }
+    portion { 1.5 }
+    tips { "ピリッと辛めに味付けするのがオススメ" }
+    reference { "https://cookpad.com/recipe/2798655" }
+    required_time { 30 }
+    popularity { 5 }
+    association :user
+    created_at { Time.current }
+  end
+
+  trait :yesterday do
+    created_at { 1.day.ago }
+  end
+
+  trait :one_week_ago do
+    created_at { 1.week.ago }
+  end
+
+  trait :one_month_ago do
+    created_at { 1.month.ago }
   end
 end
