895029dd03118dcce19f0293877a8a336bc76cd0
Ubuntu <ubuntu@ip-172-31-18-217.us-east-2.compute.internal>
2020-09-11 06:14:45 +0000

    Relationshipモデルのバリデーションとそのテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:follower, :followed] do
     name { Faker::Name.name }
     sequence(:email) { |n| "example#{n}@example.com" }
     password { 'foobar' }
