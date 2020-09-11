f9c5afb94af24be1ea2304cbd920d2dbd7c3461b
Ubuntu <ubuntu@ip-172-31-18-217.us-east-2.compute.internal>
2020-09-11 15:27:49 +0000

    ユーザーのステータスフィードを取得するfeedメソッドを完成、テスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,7 +52,10 @@ class User < ApplicationRecord
   
   # フィード一覧を取得
   def feed
-    Dish.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Dish.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
   
    # ユーザーをフォローする
