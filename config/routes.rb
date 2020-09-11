ab46cf6a4d783177cee0bb7351671225cc6e4132
Ubuntu <ubuntu@ip-172-31-18-217.us-east-2.compute.internal>
2020-09-11 13:14:16 +0000

    フォロー中、フォロワー一覧ページとそのテスト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
   get :signup,       to: 'users#new'
+  get    :login,     to: 'sessions#new'
+  post   :login,     to: 'sessions#create'
+  delete :logout,    to: 'sessions#destroy'
   resources :users do
   member do
       get :following, :followers
     end
   end
   resources :dishes
-  get    :login,     to: 'sessions#new'
-  post   :login,     to: 'sessions#create'
-  delete :logout,    to: 'sessions#destroy'
   resources :relationships, only: [:create, :destroy]
 end
