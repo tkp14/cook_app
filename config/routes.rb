45d09e6788045a0b167ff6c5947637d5b422bfe9
Ubuntu <ubuntu@ip-172-31-18-217.us-east-2.compute.internal>
2020-09-13 13:17:54 +0000

    Commentsコントローラ、ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
     end
   end
   resources :dishes
-  resources :relationships, only: %i[create destroy]
+  resources :relationships, only: [:create, :destroy]
   get :favorites,    to: 'favorites#index'
   post   "favorites/:dish_id/create"  => "favorites#create"
   delete "favorites/:dish_id/destroy" => "favorites#destroy"
+  resources :comments, only: [:create, :destroy]
 end
