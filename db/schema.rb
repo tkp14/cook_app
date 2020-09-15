47221526595753d20cbdc688794119d55da191e2
Ubuntu <ubuntu@ip-172-31-18-217.us-east-2.compute.internal>
2020-09-15 05:38:26 +0000

    通知機能のテストを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_163804) do
+ActiveRecord::Schema.define(version: 2020_09_15_045642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2020_09_14_163804) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.boolean "notification", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
