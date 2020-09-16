bc67460cdf2906cb1dcd0b67781178ad6eedbcaa
Ubuntu <ubuntu@ip-172-31-18-217.us-east-2.compute.internal>
2020-09-16 14:45:07 +0000

    Logコントローラのbefore_actionにcorrect_userを追加,クックログの追加をトップページ、マイページ、リスト一覧ページからもできるように実装

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,11 +1,14 @@
 class LogsController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user, only: :create
   
   def create
     @dish = Dish.find(params[:dish_id])
     @log = @dish.logs.build(content: params[:log][:content])
     @log.save
     flash[:success] = "クックログを追加しました！"
+    # リスト一覧ページからクックログが作成された場合、その料理をリストから削除
+    List.find(params[:list_id]).destroy unless params[:list_id].nil?
     redirect_to dish_path(@dish)
   end
 
@@ -17,5 +20,13 @@ class LogsController < ApplicationController
       flash[:success] = "クックログを削除しました"
     end
     redirect_to dish_url(@dish)
+  end
+  
+  private
+  
+  def correct_user
+    # 現在のユーザーが対象の料理を保有しているかどうか確認
+    dish = current_user.dishes.find_by(id: params[:dish_id])
+    redirect_to root_url if dish.nil?
   end
 end
