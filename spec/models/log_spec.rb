2e852434556d870b65de86ad47e6150b7e33791f
Ubuntu <ubuntu@ip-172-31-18-217.us-east-2.compute.internal>
2020-09-16 00:38:19 +0000

    バリデーションのテスト

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Log, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:log) { create(:log) }
+  
+  context "バリデーション" do
+    it "有効な状態であること" do
+      expect(log).to be_valid
+    end
+
+    it "dish_idがなければ無効な状態であること" do
+      log = build(:log, dish_id: nil)
+      expect(log).not_to be_valid
+    end
+  end
 end
