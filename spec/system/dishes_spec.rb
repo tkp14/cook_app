022847b2836f7cfa9ab684fb708ba7b49d360eb4
Ubuntu <ubuntu@ip-172-31-18-217.us-east-2.compute.internal>
2020-09-18 09:11:04 +0000

    料理編集ページから材料も編集できるように実装

--- spec/system/dishes_spec.rb
+++ spec/system/dishes_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Dishes', type: :system do
   let!(:other_user) { create(:user) }
   let!(:dish) { create(:dish, user: user) }
   let!(:dish) { create(:dish, :picture, user: user) }
+  let!(:dish) { create(:dish, :picture, :ingredients, user: user) }
   let!(:log) { create(:log, dish: dish) }
 
   describe '料理登録ページ' do
@@ -40,10 +41,10 @@ RSpec.describe 'Dishes', type: :system do
       end
 
       it "材料入力部分が10行表示されること" do
-      expect(page).to have_css 'input.ingredient_name', count: 10
-      expect(page).to have_css 'input.ingredient_quantity', count: 10
+        expect(page).to have_css 'input.ingredient_name', count: 10
+        expect(page).to have_css 'input.ingredient_quantity', count: 10
+      end
     end
-  end
 
     context '料理登録処理' do
       it '有効な情報で料理登録を行うと料理登録成功のフラッシュが表示されること' do
@@ -137,6 +138,13 @@ RSpec.describe 'Dishes', type: :system do
         expect(page).to have_content '作り方参照用URL'
         expect(page).to have_content '所要時間 [分]'
         expect(page).to have_content '人気度 [1~5]'
+        expect(page).to have_css 'p.title-ingredient-name', text: '材料（10種類まで登録可）', count: 1
+        expect(page).to have_css 'p.title-ingredient-quantity', text: '量', count: 1
+      end
+    
+      it "材料入力部分が10行表示されること" do
+        expect(page).to have_css 'input.ingredient_name', count: 10
+        expect(page).to have_css 'input.ingredient_quantity', count: 10
       end
     end
 
@@ -149,6 +157,8 @@ RSpec.describe 'Dishes', type: :system do
         fill_in '作り方参照用URL', with: 'henshu-https://cookpad.com/recipe/2798655'
         fill_in '所要時間', with: 60
         fill_in '人気度', with: 1
+        fill_in "dish[ingredients_attributes][0][name]", with: "編集-豆腐"
+        fill_in "dish[ingredients_attributes][0][quantity]", with: "編集-2個"
         attach_file 'dish[picture]', "#{Rails.root}/spec/fixtures/test_dish2.jpg"
         click_button '更新する'
         expect(page).to have_content '料理情報が更新されました！'
@@ -160,6 +170,8 @@ RSpec.describe 'Dishes', type: :system do
         expect(dish.reload.required_time).to eq 60
         expect(dish.reload.popularity).to eq 1
         expect(dish.reload.picture.url).to include 'test_dish2.jpg'
+        expect(dish.reload.ingredients.first.name).to eq "編集-豆腐"
+        expect(dish.reload.ingredients.first.quantity).to eq "編集-2個"
       end
 
       it '無効な更新' do
